2e2b1e2623782114c0af6ada24435a154dd24250
kbaumgart <keith.baumgart@gmail.com>
2021-09-03 19:39:33 -0400

    MVP

--- States/DE.js
+++ States/DE.js
@@ -1,19 +1,20 @@
 //Required Modules
-const fetch 		= require('node-fetch');
-const sql 			= require('../Workers/DB.js');
-const config 		= require('../config.json');
-const returndata 	= require('../Workers/ReturnData.js');
-const Embeds 		= require('../Workers/Embeds.js');
+const fetch = require('node-fetch');
+const sql = require('../Workers/DB.js');
+const config = require('../config.json');
+const returndata = require('../Workers/ReturnData.js');
+const Embeds = require('../Workers/Embeds.js');
 
-//DE specific variables & constants
+//DE specific constants & variables
 const advisoryURL = 'https://tmc.deldot.gov/json/advisory.json';
 const scheduleURL = 'https://deldot.gov/json/str.json';
 var advisoryCount;
 var scheduleCount;
 
-module.exports = { 
+module.exports = {
 	Pull: function Pull(bot) {
-		bot.DEChannel = bot.channels.cache.find(channel => channel.id === config.DE.Channel);
+		//bot.DEChannel = bot.channels.cache.find(channel => channel.id === config.DE.TestChannel);
+		bot.DEChannel = bot.channels.cache.find(channel => channel.id === config.DE.ClosureChannel);
 		//Fetch the DelDot Advisory Feed
 		fetch(advisoryURL)
 		.then(response => response.json())
@@ -23,16 +24,16 @@ module.exports = {
 			while (i < advisoryCount) {
 				let entry = advisoryResponse.advisories[i];
 				entry.EventType = "Advisory";
-				if ((entry.where.location.includes("LANE") === false) && (entry.where.location.includes("CLOS") === true) && (entry.where.location.includes("SHOULDER") === false) && (entry.where.location.includes("LN CLOS") === false)) {
-					sql.db.get(`SELECT * FROM DE LEFT JOIN CLOSURE ON DE.MessageID = CLOSURE.MessageID WHERE EventID = "${entry.id}"`, function (err,row) {
+				entry.Status = "New";
+				if (((entry.where.location.includes("LANE") === false) && (entry.where.location.includes("CLOS") === true) && (entry.where.location.includes("SHOULDER") === false) && (entry.where.location.includes("LN CLOS") === false)) || (entry.where.location.includes("RAMP CLOS") === true) || (entry.where.location.includes("ALL LANES CLOSED") === true)) {
+					sql.db.get(`SELECT * FROM closures WHERE EventID="${entry.id}"`, function (err,row) {
 					if (err) {
-                        console.log(err);
 						throw err;
-
+						console.log(err);
 					}
 					if (!row) {
-						bot.DEChannel.send(Embeds.DEAdvisoryClose(entry)).then(msg => { sql.db.run(`UPDATE DE SET MessageID="${msg.id}" WHERE EventID="${entry.id}"`); } );
-						sql.db.run("INSERT INTO DE (EventID, Desc, TimeStamp, EventType, AdvisoryType, Link, MessageID, Lat, Lon, Address, County) VALUES (?,?,?,?,?,?,?,?,?,?,?)", returndata.AdvisoryDE(entry));  
+						bot.DEChannel.send(Embeds.DEAdvisoryClose(entry)).then(msg => { sql.db.run(`UPDATE closures SET MessageID="${msg.id}" WHERE EventID="${entry.id}"`); } );
+						sql.db.run("INSERT INTO closures (EventID, Desc, TimeStamp, EventType, AdvisoryType, Link, MessageID, Lat, Lon, Address, County) VALUES (?,?,?,?,?,?,?,?,?,?,?)", returndata.AdvisoryDE(entry));  
 						console.log(new Date().toLocaleString() + " " + entry.id + " Inserted!");
 						
 					}
@@ -40,12 +41,11 @@ module.exports = {
 				}
 				i++;
 			}
-			//Remove closures that are no longer in the DelDot Advisory Feed
-			sql.db.each("SELECT * FROM DE WHERE EventType = 'Advisory'", function (err,row) {
+			//Update closures that were either updated or removed from the DelDot Feed
+			sql.db.each("SELECT * FROM closures WHERE EventType='Advisory'", function (err,row) {
 			if (err) {
-                console.log(err);
 				throw err;
-
+				console.log(err);
 			}
 			i=0;
 			let advisoryValid = false;
@@ -53,17 +53,28 @@ module.exports = {
 				entry = advisoryResponse.advisories[i];
 				entry.EventType = "Advisory";
 				if (row.EventID == entry.id) {
-					advisoryValid = true; //If closure is valid, do not remove from DB
+					if ((entry.where.location.includes("LANE CLOS") === false) && (entry.where.location.includes("LN CLOS") === false) && (entry.where.location.includes("RIGHT LANE") === false) && (entry.where.location.includes("LEFT LANE") === false)) {
+						advisoryValid = true; //If closure is valid, do not remove from DB
+						//If timestamp and description change, send the update
+						if ((row.TimeStamp != entry.timestamp) && (row.Desc != entry.where.location)) {
+							entry.Status = "Updated";
+							bot.DEChannel.send(Embeds.DEAdvisoryClose(entry));
+							console.log(row);
+							console.log(entry);
+							sql.db.run(`UPDATE closures SET TimeStamp="${entry.timestamp}",Desc="${entry.where.location}" WHERE EventID="${row.EventID}"`);
+							console.log(new Date().toLocaleString() + " " + row.EventID + " Updated!");
+						}
+					}
 				}
 				i++;
 			}
 			if (!advisoryValid) {
 				bot.DEChannel.send(Embeds.DEAdvisoryOpen(row));
-				sql.db.run(`DELETE FROM DE WHERE EventID="${row.EventID}"`);
+				sql.db.run(`DELETE FROM closures WHERE EventID="${row.EventID}"`);
 				console.log(new Date().toLocaleString() + " " + row.EventID + " Deleted!");
 			}
 			});
-		});
+		})
 		
 		//Fetch the DelDot Schedule Feed
 		fetch(scheduleURL)
@@ -74,16 +85,15 @@ module.exports = {
 			while (i < scheduleCount) {
 				let entry = scheduleResponse[i].str;
 				entry.EventType = "Scheduled";
-				if (entry.impactType == "Closure") {
-					sql.db.get(`SELECT * FROM DE WHERE EventID="${entry.strId}"`, function (err,row) {
+				if (((entry.impactType == "Closure") || ((entry.impactType == 'Restriction') && ((entry.construction.toUpperCase().includes("AMP CLOS")) == true) || (entry.construction.toUpperCase().includes("ROAD CLOS")) || (entry.construction.toUpperCase().includes("CLOSURE OF ROADWAY")))) && (entry.releaseId != "-1")) {
+					sql.db.get(`SELECT * FROM closures WHERE EventID="${entry.strId}"`, function (err,row) {
 					if (err) {
-						console.log(err);
 						throw err;
-
+						console.log(err);
 					}
 					if (!row) {
-						bot.DEChannel.send(Embeds.DEScheduleClose(entry)).then(msg => { sql.db.run(`UPDATE DE SET MessageID="${msg.id}" WHERE EventID="${entry.strId}"`); } );
-						sql.db.run("INSERT INTO DE (EventID, Desc, TimeStamp, EventType, Link, MessageID, Lat, Lon, Address, County) VALUES (?,?,?,?,?,?,?,?,?,?)", returndata.ScheduleDE(entry));  
+						bot.DEChannel.send(Embeds.DEScheduleClose(entry)).then(msg => { sql.db.run(`UPDATE closures SET MessageID="${msg.id}" WHERE EventID="${entry.strId}"`); } );
+						sql.db.run("INSERT INTO closures (EventID, Desc, TimeStamp, EventType, Link, MessageID, Lat, Lon, Address, County) VALUES (?,?,?,?,?,?,?,?,?,?)", returndata.ScheduleDE(entry));  
 						console.log(new Date().toLocaleString() + " " + entry.strId + " Inserted!");
 					}
 					});
@@ -91,11 +101,10 @@ module.exports = {
 				i++;
 			}
 			//Remove closures that are no longer in the DelDot Schedule Feed
-			sql.db.each("SELECT * FROM DE LEFT JOIN CLOSURE ON DE.MessageID = CLOSURE.MessageID WHERE EventType='Scheduled'", function (err,row) {
+			sql.db.each("SELECT * FROM closures WHERE EventType='Scheduled'", function (err,row) {
 			if (err) {
-				console.log(err);
 				throw err;
-
+				console.log(err);
 			}
 			i=0;
 			let scheduleValid = false;
@@ -108,17 +117,14 @@ module.exports = {
 				i++;
 			}
 			if (!scheduleValid) {
-				if (row.UserID) {
-					bot.DEChannel.guild.members.fetch(row.UserID).then(resolve => bot.DEChannel.send(`This was closed by ${resolve.displayName}`))
-				}
-				bot.DEChannel.send(Embeds.DEScheduleOpen(row));
-				sql.db.run(`DELETE FROM DE WHERE EventID="${row.EventID}"`);
+				//bot.DEChannel.send(Embeds.DEScheduleOpen(row));
+				sql.db.run(`DELETE FROM closures WHERE EventID="${row.EventID}"`);
 				console.log(new Date().toLocaleString() + " " + row.EventID + " Deleted!");
 			}
 			});
 		})
 		.catch(err => {
-		  console.log(err);
+		  console.log(new Date().toLocaleString() + err);
 		});
 	}
 };
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let NJ = require('./States/NJ.js');
 let NY = require('./States/NY.js');
 let PA = require('./States/PA.js');
 // store commands in own folder, figure out good way to require all files within that folder
-let closures = require('./commands/closures.js');
-let reactionadd = require('./events/reactionadd.js');
+//let closures = require('./commands/closures.js');
+//let reactionadd = require('./events/reactionadd.js');
 // ---------------------------------------------
 const client = new discord.Client();
 // -------config file
@@ -30,17 +30,17 @@ client.on('ready', () => {
 
 // Command handler - more to come in this section
 client.on('message', (message) => {
-  if (message.content.startsWith('ping')) {
+  if (message.content === ('ping')) {
     message.channel.send('pong!');
   } else if (message.content.startsWith('$mention')) {
     message.channel.send('hello there <@729443441347526656>')
   }
     else if ((message.content.startsWith('$reload') && (config.ownerID.includes(message.author.id)))) { 
-    delete require.cache[require.resolve('./commands/closures.js')];
+    //delete require.cache[require.resolve('./commands/closures.js')];  future enhancement
 				delete require.cache[require.resolve('./Workers/Embeds.js')];
 				delete require.cache[require.resolve('./Workers/ReturnData.js')];
 				delete require.cache[require.resolve('./States/DE.js')];
-        delete require.cache[require.resolve('./events/reactionadd.js')];
+        //delete require.cache[require.resolve('./events/reactionadd.js')]; future enhancement
 				closures = require('./commands/closures.js');
         DE = require('./States/DE.js');
         PA = require('./States/PA.js');
@@ -49,36 +49,7 @@ client.on('message', (message) => {
         reactionadd = require('./events/reactionadd.js');
 				message.channel.send("Modules Reloaded.");
 
-  } else
-  if (message.content.startsWith('!export')) {
-    let args = message.content.trim().toUpperCase().split((/ +/g));
-    const command = args.shift().toLowerCase();
-    closures.CSV(client, message, args);
-    console.log(command, args);
-  } else 
-  if (message.content.startsWith('!close')) {
-    let args = message.content.trim().toUpperCase().split((/ +/g));
-    const command = args.shift().toLowerCase();
-    console.log(args);
-    sql.db.get(`SELECT * FROM ${args[0]} WHERE EventID = "${args[1]}"`, (err, row) => {
-      if (err) throw err;
-      if (!args[1]) { message.channel.send('Improper format, !closure <state> <event ID>');}
-      console.log(`Event ${args[1]} entered into database and assigned to ${message.author.id}`);
-      message.channel.send(`Event ${args[1]} has been assigned. You will be notified once the roadway reopens.`);
-      sql.db.run(`INSERT INTO CLOSURE VALUES ("${row.MessageID}", "${message.author.id}", "${message.channel.id}", "${message.guild.id}", "${new Date()}")`);
-    });
-  }
-});
-
-client.on('messageReactionAdd', (reaction, user) => {
-  const emoji = reaction.emoji.name;
-if (reaction.message.author.bot) {
-  if ((config.closure_emoji.includes(emoji)) || (config.closure_emoji.includes(reaction.emoji.id))) {
-    reactionadd.associateID(reaction, 1);
-  }
-  else if ((config.schedule_eomji.includes(emoji)) || (config.closure_emoji.includes(reaction.emoji.id))) {
-    reactionadd.associateID(reaction, 0);
-  }}
+  } 
 });
 
 // Run the update functions every minute
